d63aded9029b7db7b299a2206121bfd646854c70
Gregory Maldonado <gmaldona@oswego.edu>
2024-02-20 18:53:02 -0500

    clean up

--- gitpopd.sh
+++ gitpopd.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
+######### GM ########################################################### 80 ####
+#                                                                              #
+# author: GM                                                                   #
+# email:  gmaldonado@cs.binghamton.edu                                         #
+# date:   2023-12-05                                                           #
+#                                                                              #
+# gitpopd.sh -                                                                 #
+#                                                                              #
+# Similar to GNU popd where a user that pushd directory, can pop the directory #
+# stack to be placed in original directory.                                    #
+# Gitpushd is designed for frequent branch switching on a git project.         #
+#                                                                              #
+# Usage:                                                                       #
+# A branch that has been previous "pushed" on the stack, can pop the branch    #
+# to be placed in the original working branch. The stacks are project specific #
+# so pushed branches are mixed with each other.                                #
+#                                                                              #
+######### GM ########################################################### 80 ####
 
-# author: Gregory Maldonado
-# date:   2023-12-05
+project=$(git worktree list | cut -d ' ' -f 1)
+[ -n "$project" ] && project=$(echo $project | xargs basename) || exit 1
 
-# gitpopd.sh -
-
-# Similar to GNU popd where a user that pushd a directory, can pop the directory
-# stack to be placed in original directory.
-# Gitpushd is designed for frequent branch switching on a git project.
-
-# Usage:
-# A branch that has been previous "pushed" on the stack, can pop the branch
-# to be placed in the original working branch. The stacks are project specific
-# so pushed branches are mixed with each other.
-
-gitroot=$(git worktree list | cut -d ' ' -f 1)
-
-if [ -n $gitroot ]; then 
-   exit 1
-fi 
-
-project=$(echo $gitroot | xargs basename)
-
-target="$HOME/.gitpushd/$project"
+target="$HOME/.gitpushd/$project" && [ ! -d "$target" ] && exit 1
 branch=$(git rev-parse --abbrev-ref HEAD)
-
-if [ ! -d $target ]; then
-   exit 1
-fi
+prevbr="$branch"
 
 pushd $target >/dev/null
 
-branch=$(tail ~/.gitpushd/cis-554/.gitpushd)
-sed -i '' -e '$ d' .gitpushd # pop top of the branch stack
+# pop top of the branch stack
+branch=$(tail "$target") && git checkout $branch
+
+if [ $(git rev-parse --abbrev-ref HEAD) != "$prevbr" ]; then
+   echo $branch
+   sed -i '' -e '$ d' .gitpushd 
+fi
 
 popd >/dev/null
 
-git checkout $branch && echo $branch
-
+######### GM ########################################################### 80 ####
--- gitpushd.sh
+++ gitpushd.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
+######### GM ########################################################### 80 ####
+#                                                                              #
+# author: GM                                                                   #
+# email:  gmaldonado@cs.binghamton.edu                                         #
+# date:   2023-12-05                                                           #
+#                                                                              #
+# gitpushd.sh -                                                                #
+#                                                                              #
+# Similar to GNU pushd where a user can pushd a directory                      #
+# to be later "popped". Stack based directory tracking.                        #
+# Gitpushd is designed for frequent branch switching on a git project.         #
+#                                                                              #
+# Usage:                                                                       #
+# The current working branch can be "pushed" onto a stack. Users switches to a #
+# new branch and later "pop" the stack to be placed back into the original     #
+# branch.                                                                      #
+#                                                                              #
+# Arguments:                                                                   #
+#                                                                              # 
+# --clear | --clean           clears content out of .gitpushd and .history for #
+#                             the given project context                        #
+#                                                                              #
+# --history                   displays history of "pushed" branches for the    #
+#                             given project context                            #
+#                                                                              #
+######### GM ########################################################### 80 ####
 
-# author: Gregory Maldonado
-# date:   2023-12-05
-
-# gitpushd.sh -
-
-# Similar to GNU pushd where a user can pushd a directory
-# to be later "popped". Stack based directory tracking.
-# Gitpushd is designed for frequent branch switching on a git project.
-
-# Usage:
-# The current working branch can be "pushed" onto a stack. User may switch to a
-# new branch and later "pop" the stack to be placed back into the original
-# branch. The stacks are project specific so pushed branches are mixed with each
-# other.
-
-if [ ! -d $HOME/.gitpushd ]; then
-   mkdir $HOME/.gitpushd
-fi
-
-gitroot=$(git worktree list | cut -d ' ' -f 1)
-if [ -n $gitroot ]; then 
-   exit 1
-fi 
-
-project=$(echo $gitroot | xargs basename)
+project=$(git worktree list | cut -d ' ' -f 1)
+[ -n "$project" ] && project=$(echo $project | xargs basename) || exit 1
 
 target="$HOME/.gitpushd/$project"
 branch=$(git rev-parse --abbrev-ref HEAD)
 
-if [ ! -d $target ]; then
-   mkdir -p $target
-   touch $target/.gitpushd
+if [ ! -d "$target" ]; then
+   mkdir -p "$target"
+   touch    "$target/.gitpushd"
+   touch    "$target/.history"
 fi
 
-pushd "$target" /dev/null >/dev/null
+if [ "$1" == "--history" ]; then
+   cat "$target/.history"
+elif [ "$1" == "--clear" ] || [ "$1" == "--clean" ]; then
+   cat /dev/null > "$target/.gitpushd" && cat /dev/null > "$target/.history"
+else
+   pushd "$target" >/dev/null
 
-# push to the top of the branch stack
-grep -q $branch .gitpushd && : || echo $branch >>.gitpushd
-echo $branch
+   # push to the top of the branch stack
+   grep -q $branch .gitpushd && : || echo $branch >>.gitpushd
+   grep -q $branch .history  && : || echo $branch >>.history
+   
+   echo $branch && popd >/dev/null
+fi
 
-popd >/dev/null
+######### GM ########################################################### 80 ####
